f7858ca3935b752fe14854029ff27baf773665e8
zirbem07 <maxwell.zirbel@gmail.com>
2014-06-30 09:00:52 -0700

    updated instructions for tutorial

--- tutorialCtrl.js
+++ tutorialCtrl.js
@@ -1,5 +1,5 @@
 //Could declare angular.module right here
-function tutorialCtrl($scope) {  //be sure to include the scope perameter. whatever scope is...
+function tutorialCtrl($scope) {  //be sure to include the scope parameter. whatever scope is...
 
 	$scope.superhero = [{name: "Spiderman",
 						type: "hero"}];
@@ -7,7 +7,7 @@ function tutorialCtrl($scope) {  //be sure to include the scope perameter. whate
 	$scope.types = '';
 	//make sure this function has the same name as the ng-click attribute associated with your add hero button
 	/* ex: $scope.addHero */ = function(){
-		if($scope. /*ng-model attr of name input field*/ || $scope. /*ng-model attr of type input field*/ ){
+		if($scope. /*ng-model attr of 'name' input field*/ || $scope. /*ng-model attr of 'type' input field*/ ){
 			//add something here to clear the name field, maybe just modify the item you added?
 		} else {
 			$scope.alerts = true;
